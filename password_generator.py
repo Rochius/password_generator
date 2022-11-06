3235bb6d6b4b0f99b06d5334740dc4cd12028e56
Rocio Morales <99517293+Rochius@users.noreply.github.com>
2022-11-06 20:49:35 -0300

    :sparkles: Feat: Show Paswords option added to password generator menu

--- password_generator.py
+++ password_generator.py
@@ -39,6 +39,18 @@ def generate_password(key):
         char_list.append(character)
     final_password = desorer_list(char_list, password_length)
     passwords_and_apps[key]= final_password
+    print(f'\nYour {key} password is: {passwords_and_apps[key]}')
+    print('Password succefully added!!')
+
+def is_empty(data_structure):
+    if data_structure:
+        return False
+    else:
+        return True
+
+def show_passwords():
+    for key, value in passwords_and_apps.items():
+        print(f'{key}: {value}')
 
 
 def menu():
@@ -47,30 +59,39 @@ def menu():
     while b:
         os.system("cls")
         print(f'Hi {user_name}!!')
-        print(' 1) Generate Password\n 2) Show passwprds \n 0) Exit')
+        print(' 1) Generate Password\n 2) Show passwords \n 0) Exit')
         op = input('Please, select an option (1, 2, 0): ')
+
         if op == '1':
-            app_name = input('Please, indicate which app the password will be for:')
+            app_name = input('Please, indicate which app the password will be for: ')
             if app_name in passwords_and_apps:
-                generate_option = input(f'You already have a {app_name} password, Do you want to overwrite it? y/n')
+                generate_option = input(f'You already have a {app_name} password, Do you want to overwrite it? (y/n): ')
                 if generate_option.lower() == 'y':
                     generate_password(app_name)
-                    print('Password succefully added!!')
                     pause_system()
                 elif generate_option.lower() != 'y' and generate_option.lower() != 'n':
                     print('That is not an available option...')
                     pause_system()
             else:
                 generate_password(app_name)
-                print('Password succefully added!!')
+                
                 pause_system()
+
         elif op == '2':
-            pass
+            if is_empty(passwords_and_apps):
+                print('You do not have any passwords...')
+                pause_system()
+            else:
+                print('\nThis are your passwords:')
+                show_passwords()
+                pause_system()
+
         elif op == '0':
             print('Thank you for use our system!!')
             b = False
         else:
             print("That is not an available option, please try again")
+            pause_system()
         
 
 menu()
