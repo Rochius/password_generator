ac188cf24fdc82586b4712d2599853293e9ae882
Rocio Morales <99517293+Rochius@users.noreply.github.com>
2022-11-10 17:43:20 -0300

    :sparkles: Feat: get password function with files

--- password_generator.py
+++ password_generator.py
@@ -10,24 +10,11 @@ passwords_and_apps = {}
 
 FILE_NAME = 'Passwords.txt'
 
-
-def change_to_string(passwords_data_structure):
-    passwords_in_string = ''
-    for key, value in passwords_data_structure.items():
-        passwords_in_string += f'K: ${key}, V: ${value}\n'
-    return passwords_in_string
-
-def store_passwords(passwords):
-    file = open(FILE_NAME, 'a')
-    file.write(change_to_string(passwords))
-    print('Succefully stored')
-
-
 def pause_system():
     print("\nPress a key to continue...")
     msvcrt.getch()
 
-
+#GENERATE PASSWORD
 def obligatory_characters():
     obl_chars = []
     obl_chars.append(random.choice(capital_letters))
@@ -40,7 +27,7 @@ def desorer_list (char_list, password_length):
     char_list_joined = ''.join(char_list)
     return char_list_joined
 
-def generate_password(key):
+def generate_password():
     char_type = [1,2,3]
     password_length = random.randint(9,16) - 3
     char_list = obligatory_characters()
@@ -54,18 +41,61 @@ def generate_password(key):
             character = str(random.choice(numbers))
         char_list.append(character)
     final_password = desorer_list(char_list, password_length)
-    passwords_and_apps[key]= final_password
-    print(f'\nYour {key} password is: {passwords_and_apps[key]}')
-    print('Password succefully added!!')
+    # passwords_and_apps[key]= final_password
+    # print(f'\nYour {key} password is: {passwords_and_apps[key]}')
+    # print('Password succefully added!!')
+    return final_password
+
+
+
+
+def change_to_string(password , app_name):
+    passwords_in_string = f'{app_name}: {password}\n'
+    return passwords_in_string
+
+def app_exists(app_name):
+    with open(FILE_NAME, 'r') as file:
+        for line in file:
+            words = line.split(': ')
+            if app_name.lower() == words[0].lower():
+                return True
+    return False
+
+def store_passwords(password, app_name):
+    file = open(FILE_NAME, 'a')
+    file.write(change_to_string(password , app_name))
+    file.close()
+    print('Succefully stored')
+
 
 def show_passwords():
-    for key, value in passwords_and_apps.items():
-        print(f'{key}: {value}')
+    with open(FILE_NAME , 'r') as file:
+        for line in file:
+            print(line)
 
+def replace_passwords(password, app_name):
+    with open(FILE_NAME, 'r') as file:
+        x = file.read()
+    lines = x.split('\n')
+    for i in range(0, len(lines)):
+        if app_name.lower() in lines[i].lower():
+            lines[i] = change_to_string(password, app_name).rstrip()
+    x = '\n'.join(lines)
+
+    with open(FILE_NAME , 'w') as file:
+        file.write(x)
+
+def file_is_empty():
+    with open(FILE_NAME, 'r') as file:
+        file.seek(0, os.SEEK_END)
+        isempty = file.tell() == 0
+        file.seek(0)
+        return isempty
 
 def menu():
     b = True
     user_name = input('Please, enter your name: ')
+
     while b:
         os.system("cls")
         print(f'Hi {user_name}!!')
@@ -74,21 +104,23 @@ def menu():
 
         if op == '1':
             app_name = input('Please, indicate which app the password will be for: ')
-            if app_name in passwords_and_apps:
+            if app_exists(app_name):
                 generate_option = input(f'You already have a {app_name} password, Do you want to overwrite it? (y/n): ')
+                
                 if generate_option.lower() == 'y':
-                    generate_password(app_name)
+                    password = generate_password()
+                    replace_passwords(password , app_name)
                     pause_system()
                 elif generate_option.lower() != 'y' and generate_option.lower() != 'n':
                     print('That is not an available option...')
                     pause_system()
             else:
-                generate_password(app_name)
-                
+                password = generate_password()
+                store_passwords(password , app_name)
                 pause_system()
 
         elif op == '2':
-            if passwords_and_apps:
+            if not file_is_empty():
                 print('\nThis are your passwords:')
                 show_passwords()
                 pause_system()
@@ -108,4 +140,4 @@ def menu():
 
 menu()
 
-store_passwords(passwords_and_apps)
+
